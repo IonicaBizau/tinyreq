c7a0b5aab6eac8dc4d52fe37a78ba338c32655b5
Ionică Bizău <bizauionica@gmail.com>
2017-08-11 09:10:23 +0530

    Fix the Promises support

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 // Dependencies
 var Assert = require("assert")
   , TinyReq = require("../lib")
+  , http = require("http")
   ;
+
+mocha.setup({ timeout: 5000 });
 
 // http
 it("should support http requests", function (cb) {
@@ -9,6 +12,16 @@ it("should support http requests", function (cb) {
         Assert.equal(err, null);
         Assert.equal(!!/this domain/gi.test(body), true);
         cb();
+    });
+});
+
+// http
+it("should support promises", function (cb) {
+    TinyReq("http://example.com/").then(body => {
+        Assert.equal(!!/this domain/gi.test(body), true);
+        cb();
+    }).catch(err => {
+        cb(err);
     });
 });
 
